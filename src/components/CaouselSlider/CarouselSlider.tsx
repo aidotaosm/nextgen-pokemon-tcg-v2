2e4775032d8062795e0553fb0370b1ab98ff12a0
Osama Almas Rahman <o.almas@hotmail.com>
2024-01-08 02:11:29 +0600

    homepage image unoptmized

--- src/components/CaouselSlider/CarouselSlider.tsx
+++ src/components/CaouselSlider/CarouselSlider.tsx
@@ -101,7 +101,7 @@ const CarouselSlider: FunctionComponent<CarouselSliderProps> = ({
                 height={1024}
                 blurDataURL={defaultBlurImage}
                 className="h-100 w-100"
-                lqImageUnOptimize={false}
+                lqImageUnOptimize={true}
                 lqEagerLoading={"eager"}
                 //   hqEagerLoading={"lazy"}
               />
